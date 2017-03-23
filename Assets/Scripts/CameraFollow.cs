5a2064a6ea00fbab706476072fd9ffe3a7ddbfcf
Covina <jabs303@gmail.com>
2017-03-23 00:02:30 -0700

    Section 1, Lecture 9 - Set up Enemies

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+
+	// Get player position
+	// Calculate offset from camera
+	// Move camera on LateUpdate into position
+
+	// Get the player object
+	private GameObject playerPos;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+
+		// Get Player
+		playerPos = GameObject.FindGameObjectWithTag("Player");
+		Debug.Log("Player: " + playerPos);
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+
+	// Moev the Camera
+	void LateUpdate ()
+	{
+
+		// move the camera arm based on the offset
+		transform.position = playerPos.transform.position;
+
+	}
+}

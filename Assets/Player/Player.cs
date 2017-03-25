fb3e47014ce80b93cda6c0b4c0dd2fd32d1f2f0a
Covina <jabs303@gmail.com>
2017-03-25 10:04:23 -0700

    End of Section 2, lecture 27:  Added custom health bar, updated art asset

---
+++ Assets/Player/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	
+	[SerializeField] private float maxHealthPoints = 100f;
+	private float currentHealthPoints = 100f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	public float healthAsPercentage {
+		get {
+			return currentHealthPoints / maxHealthPoints;
+		}
+	}
+
+
+
+
+}
--- Assets/Player/PlayerMovement.cs
+++ Assets/Player/PlayerMovement.cs
@@ -7,18 +7,24 @@ public class PlayerMovement : MonoBehaviour
 {
     ThirdPersonCharacter thirdPersonCharacter;   // A reference to the ThirdPersonCharacter on the object
     CameraRaycaster cameraRaycaster;
-    Vector3 currentClickTarget;
+
+    private Vector3 currentDestination;
+    private Vector3 clickPoint;
+
 
     private bool isInDirectMode = false;	
 
-    [SerializeField] float walkMoveStopRadius = 0.20f;
+	[SerializeField] float walkMoveStopRadius = 0.20f;
+	[SerializeField] float attackMoveStopRadius = 2.0f;
+
+
 
         
     private void Start()
     {
         cameraRaycaster = Camera.main.GetComponent<CameraRaycaster>();
         thirdPersonCharacter = GetComponent<ThirdPersonCharacter>();
-        currentClickTarget = transform.position;
+        currentDestination = transform.position;
     }
 
     // Fixed update is called in sync with physics
@@ -31,7 +37,7 @@ public class PlayerMovement : MonoBehaviour
 			Debug.Log("G Pressed, switching to " + isInDirectMode);
 
 			// SJ - default out the currentClickTarget so the player doesnt return to last known click point
-			currentClickTarget = transform.position;
+			currentDestination = transform.position;
 		}
 
 		if (isInDirectMode) {
@@ -74,23 +80,36 @@ public class PlayerMovement : MonoBehaviour
 	{
 		if (Input.GetMouseButton (0)) {
 			print ("Cursor raycast hit" + cameraRaycaster.hit.collider.gameObject.name.ToString ());
+
+
 			// SJ - added to only register new location if target point is walkable
 			// SJ - Changed from IF to SWITCH afterward
+
+			clickPoint = cameraRaycaster.hit.point;		// store the mouse click point
+
 			switch (cameraRaycaster.currentLayerHit) {
 				case Layer.Walkable:
-					currentClickTarget = cameraRaycaster.hit.point;
-					// So not set in default case
+					currentDestination = ShortDestination(clickPoint, walkMoveStopRadius);
 					break;
 				case Layer.Enemy:
-					Debug.Log ("Clicked on Enemy");
+					currentDestination = ShortDestination(clickPoint, attackMoveStopRadius);
 					break;
 				default:
 					Debug.Log ("Default reached in Switch");
 					return;
 			}
 		}
+
+		WalkToDestination ();
+
+	}
+
+	// SJ - Extracted method
+	// move the character
+	private void WalkToDestination ()
+	{
 		// SJ - get the distance of the move between current position and click position
-		var playerToClickPoint = currentClickTarget - transform.position;
+		var playerToClickPoint = currentDestination - transform.position;
 		// SJ - Move until we get within the radius then stop
 		if (playerToClickPoint.magnitude >= walkMoveStopRadius) {
 			thirdPersonCharacter.Move (playerToClickPoint, false, false);
@@ -100,5 +119,40 @@ public class PlayerMovement : MonoBehaviour
 			thirdPersonCharacter.Move (Vector3.zero, false, false);
 		}
 	}
+
+
+	private Vector3 ShortDestination (Vector3 destination, float shortening)
+	{
+			// calculate the subtraction vector
+			Vector3 reductionVector = (destination - transform.position).normalized * shortening;	
+
+			// return the shorter vector.
+			return (destination - reductionVector);
+
+	}
+
+
+	void OnDrawGizmos()
+	{
+
+		print("Gizmos Draw");
+
+		Gizmos.color = Color.black;
+		Gizmos.DrawLine(transform.position, currentDestination);
+
+		Gizmos.DrawSphere(currentDestination, 0.1f);	// draw sphere at end of line
+		Gizmos.DrawSphere(clickPoint, 0.2f);
+
+		// Draw Attack Sphere
+		Gizmos.color = new Color(255f, 0f, 0f, 0.5f);
+		Gizmos.DrawWireSphere(transform.position, attackMoveStopRadius);
+
+
+	}
+
+
+
+
+
 }
 

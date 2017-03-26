bcda56b85d48be104caa21125beb20b60c535c2d
Covina <jabs303@gmail.com>
2017-03-25 22:31:03 -0700

    Section 3, Lecture 39;  Updated Windmill and added Z Rotation spin from script.

---
+++ Assets/Utility/SpinMe.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinMe : MonoBehaviour {
+
+	[SerializeField] float xRotationsPerMinute = 1f;
+	[SerializeField] float yRotationsPerMinute = 1f;
+	[SerializeField] float zRotationsPerMinute = 1f;
+	
+	void Update () {
+
+
+		// 360 degrees * rotationsPerMinute = total degrees per minute
+		// TotalDegree / 60 = degrees per second
+		// DPS * Time.Deltatime = degree movement per frame
+
+
+        float xDegreesPerFrame = 0; // TODO COMPLETE ME
+        transform.RotateAround (transform.position, transform.right, xDegreesPerFrame);
+
+		float yDegreesPerFrame = 0; // TODO COMPLETE ME
+        transform.RotateAround (transform.position, transform.up, yDegreesPerFrame);
+
+		float zDegreesPerFrame = ((360 * zRotationsPerMinute) / 60) * Time.deltaTime; // TODO COMPLETE ME
+        transform.RotateAround (transform.position, transform.forward, zDegreesPerFrame);
+	}
+}
